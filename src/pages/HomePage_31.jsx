6841c01a37eca7432ca5b7ddf2c0b0638047317e
hang0727 <213417131@o365.tku.edu.tw>
2025-10-16 15:23:50 +0800

    add tailwindcss support

--- src/pages/HomePage_31.jsx
+++ src/pages/HomePage_31.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage_31 = () => {
   return (
-    <div>
-      <h1>HomePage_31</h1>
-      <h2>hang0727, 213417131</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-purple-700'>HomePage_31</h1>
+      <h2 className='text-xl'>hang0727, 213417131</h2>
     </div>
   );
 };
