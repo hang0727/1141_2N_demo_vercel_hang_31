cf819cd78c7d3cb85af6dffaaf627ba89ab691ca
hang0727 <213417131@o365.tku.edu.tw>
2025-10-16 14:25:40 +0800

    first commit

---
+++ src/pages/HomePage_31.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage_31 = () => {
+  return (
+    <div>
+      <h1>HomePage_31</h1>
+      <h2>hang0727, 213417131</h2>
+    </div>
+  );
+};
+
+export default HomePage_31;
